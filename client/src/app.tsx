c127dd01972ddc955f09387996435e5acd355c9d
Aradhna <ajp2005@cs.washington.edu>
2025-05-08 14:07:29 -0700

    Added routes to view pages

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,20 @@
 import {createRoot} from 'react-dom/client';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router";
 import PetView from './pet/view';
+import PomodoroView from './pomodoro/view';
 
 const root = createRoot(document.body);
-root.render(<PetView />);
+root.render(
+<BrowserRouter>
+  <nav>
+    <Link to="/view">View</Link>
+    <Link to="/petView">Pet View</Link>
+    <Link to="/pomodoroView">Pomodoro View</Link>
+    <Link to="/marketView">Market View</Link>
+  </nav>
+  <Routes>
+      <Route path="/petView" element={<PetView />} />
+      <Route path="/pomodoroView" element={<PomodoroView />} />
+      {/* <Route path="/marketView" element={<MarketView />} /> */}
+  </Routes>
+</BrowserRouter>);
