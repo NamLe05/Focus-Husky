02da3f37d1a0a62479ce10e171b2e6356cb1e953
Tony <tonytpunnacherry@gmail.com>
2025-05-12 21:33:23 -0700

    Added Task link to the UI

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,20 +1,23 @@
 import {createRoot} from 'react-dom/client';
-import {HashRouter, Routes, Route, Link} from "react-router";
+import {HashRouter, Routes, Route, Link} from 'react-router';
 import PetView from './pet/view';
 import PomodoroView from './pomodoro/view';
+import TaskView from './tasks/view';
 
 const root = createRoot(document.body);
 root.render(
-<HashRouter>
-  <nav>
-    <Link to="/view">View</Link>
-    <Link to="/petView">Pet View</Link>
-    <Link to="/pomodoroView">Pomodoro View</Link>
-    <Link to="/marketView">Market View</Link>
-  </nav>
-  <Routes>
+  <HashRouter>
+    <nav>
+      <Link to="/view">View</Link>
+      <Link to="/petView">Pet View</Link>
+      <Link to="/pomodoroView">Pomodoro View</Link>
+      <Link to="/marketView">Market View</Link>
+    </nav>
+    <Routes>
       <Route path="/petView" element={<PetView />} />
       <Route path="/pomodoroView" element={<PomodoroView />} />
+      <Route path="/taskView" element={<TaskView />} />
       {/* <Route path="/marketView" element={<MarketView />} /> */}
-  </Routes>
-</HashRouter>);
+    </Routes>
+  </HashRouter>,
+);
